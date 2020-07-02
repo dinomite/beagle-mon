659b965e9e40a680990e017136bc785d8f3edb8d
Drew Stephens <drew@dinomite.net>
2020-07-02 06:36:38 -0400

    Add temp sensor

--- monitor-environment.py
+++ monitor-environment.py
@@ -84,7 +84,8 @@ sensors = {
     'desk': (BMP085.BMP085(mode=BMP085.BMP085_ULTRAHIGHRES), lambda x: x - 10 ),
     'outside': (get_1w_sensor("000005aba36c"), lambda x: x + 3),
     'vent': (get_1w_sensor("000005ab8e9c"), lambda x: x + 1),
-    'basement_hvac_input': (get_1w_sensor("0416565308ff"), lambda x: x)
+    'basement_hvac_input': (get_1w_sensor("0416565308ff"), lambda x: x),
+    'basement_hvac_output': (get_1w_sensor("000008e3a7d2"), lambda x: x)
 }
 logger.debug("Sensor handles created")
 
